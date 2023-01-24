3a20844f8bcb5a000cad5442816bc5206a6292f3
Matthieu Picard <77137944+Morr-valir@users.noreply.github.com>
2023-01-24 12:56:03 +0400

    create crud user

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Book;
 use App\Entity\Category;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -28,8 +29,12 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('Gestions des livres','fas fa-bowl-rice',Book::class);
-        yield MenuItem::linkToCrud('Gestion des catégories','fas fa-bowl-rice',Category::class);
+        yield MenuItem::section('Gestion des livres');
+        yield MenuItem::linkToCrud('livres','fas fa-bowl-rice',Book::class);
+        yield MenuItem::linkToCrud('catégories','fas fa-bowl-rice',Category::class);
+        yield MenuItem::section('Gestion des réservation');
+        yield MenuItem::section('Gestion utilisateur');
+        yield MenuItem::linkToCrud('Utilisateurs','fas fa-bowl-rice',User::class);
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
     }
 }
