d5e70f28ab2dcbee05e509a93a19e9f381c72261
Matthieu Picard <77137944+Morr-valir@users.noreply.github.com>
2023-01-24 12:38:49 +0400

    add entity crud category and fixed to string category

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Book;
+use App\Entity\Category;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -27,7 +28,8 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('Listes des livres','fas fa-bowl-rice',Book::class);
+        yield MenuItem::linkToCrud('Gestions des livres','fas fa-bowl-rice',Book::class);
+        yield MenuItem::linkToCrud('Gestion des catégories','fas fa-bowl-rice',Category::class);
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
     }
 }
