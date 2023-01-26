1301a369a8ef561f8bfd1c70e0768b24ec2eedb6
Azioogh <95343833+Azioogh@users.noreply.github.com>
2023-01-26 16:04:35 +0400

    Trigger + Procedure fonctionnel et BookingCrudController user experience avec des alerts

--- triggers/stateBookAvailable.sql
+++ triggers/stateBookAvailable.sql
@@ -1,3 +1,3 @@
-CREATE TRIGGER `stateBookAvailable` AFTER INSERT ON `booking_book`
- FOR EACH ROW UPDATE book SET book.is_available = 0 
-WHERE book.id = NEW.book_id
+CREATE TRIGGER `stateBookAvailable` AFTER DELETE ON `booking_book`
+ FOR EACH ROW UPDATE book SET book.is_available = 1 
+WHERE book.id = OLD.book_id
