e97e8f388e8de693ca0529f4bccf2849451c268d
Nicolas HOARAU <h.nico.sully@gmail.com>
2023-01-27 14:51:41 +0400

    Mise en forme du formulaire de modification du profile utilisateur

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\User;
 use DateTime;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -15,22 +16,21 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email',TextType::class,[
+            ->add('email', TextType::class, [
                 'label' => "Email"
             ])
-            ->add('firstname',TextType::class,[
+            ->add('firstname', TextType::class, [
                 'label' => "Prénom"
             ])
-            ->add('lastname',TextType::class,[
+            ->add('lastname', TextType::class, [
                 'label' => "Nom"
             ])
-            ->add('tel',TextType::class,[
+            ->add('tel', TextType::class, [
                 'label' => "Tel"
             ])
-            ->add('birthDate',DateType::class,[
+            ->add('birthDate', BirthdayType::class, [
                 'label' => "Date de naissance"
-            ])
-        ;
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
