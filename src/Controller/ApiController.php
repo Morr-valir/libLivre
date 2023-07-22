a88748ccef40080a8c62e79c308553551460cca2
Matthieu Picard <77137944+Morr-valir@users.noreply.github.com>
2023-07-22 19:17:09 +0400

    add controller Api and route book to category

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -4,8 +4,11 @@ namespace App\Controller;
 
 use App\Repository\BookRepository;
 use App\Repository\CategoryRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
 
 class ApiController extends AbstractController
 {
@@ -17,5 +20,24 @@ class ApiController extends AbstractController
     {
         $this->repoBooks = $bookRepository;
         $this->repoCategory = $categoryRepository;
+    }
+
+    #[Route('/api/categories/book/{id}', name: 'api_collection_booksCategory', methods: ['GET'])]
+    public function getBooksToCategories(SerializerInterface $serializer, Request $request): Response
+    {
+        $idBook = $request->get('id');
+        $getCollectionBooks = $this->repoBooks->findByCategory($idBook);
+
+        if($getCollectionBooks === null){
+            $response = new Response(null,600,[
+            'Content-Type' => 'application/json',
+            ]);
+            return $response;
+        }
+        $jsonCollectionBooks = $serializer->serialize($getCollectionBooks,'json');
+        $response = new Response($jsonCollectionBooks,200, [
+            'Content-Type' => 'application/json',
+        ]);
+        return $response;
     }
 }
