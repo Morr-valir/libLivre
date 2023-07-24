efd5c8d215921812238b68b48cf83dc39ccc81d3
Matthieu Picard <77137944+Morr-valir@users.noreply.github.com>
2023-07-24 20:13:08 +0400

    Update User.php

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiProperty;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\UserRepository;
@@ -32,6 +33,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?int $id = null;
 
     #[ORM\Column(length: 180, unique: true)]
+    #[ApiProperty(identifier:true)]
     private ?string $email = null;
 
     #[ORM\Column]
