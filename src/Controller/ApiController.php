8b2f12ecbbee4a76c25252b05f1afd025037e734
Matthieu Picard <77137944+Morr-valir@users.noreply.github.com>
2023-07-26 15:45:40 +0400

    Update ApiController.php

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -7,14 +7,12 @@ use ApiPlatform\Metadata\ApiResource;
 use App\Repository\BookingRepository;
 use App\Repository\CategoryRepository;
 use ApiPlatform\Metadata\GetCollection;
-use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
-use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 
 class ApiController extends AbstractController
 {
@@ -74,8 +72,7 @@ class ApiController extends AbstractController
      * @return JSON array
      */
     #[Route('/api/booking/user/{id}', name: 'api_collection_bookingUser', methods: ['GET'])]
-    #[IsGranted('ROLE_USER')]
-    public function getBookingToUSerId(NormalizerInterface $normalizerInterface, Request $request): Response
+    public function getBookingToUSerId(SerializerInterface $serializer, Request $request): Response
     {
         $userId = $request->get('id');
         $getBookingList = $this->bookingRepository->findBookingUser($userId);
@@ -86,10 +83,8 @@ class ApiController extends AbstractController
             ]);
             return $response;
         }
-        $jsonBookingUser = $normalizerInterface->normalize($getBookingList, null,['group' => ['GetBooking']]);
-        $encodeJson = json_encode($jsonBookingUser);
-
-        $response = new Response($encodeJson,200,[
+        $jsonBookingUser = $serializer->serialize($getBookingList,'json');
+        $response = new Response($jsonBookingUser,200,[
             'Content-Type' => 'application/json',
         ]);
         return $response;
